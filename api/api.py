b169b5417f7411591b9cef17a3cffaf7124d1704
Keshawn <strongkeshawn047@gmail.com>
2022-06-14 03:03:53 +0000

    Edited api.py

--- api/api.py
+++ api/api.py
@@ -1,12 +1,16 @@
 import flask
 from flask import request, jsonify
+import app as ap
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/", methods=["GET"])
-def home(): 
-    return "<h1> Hi </h1>" 
+@app.route("/api/v1/resources/func/setValues", methods=["GET"])
+def setValues():
+    if 'rownum' in request.args and 'name' in request.args:     
+        ap.setAllValues(request.args['rownum'], request.args['name'])
+    else: 
+        return "Error: No rownum(Row Number) and name specified"
 
 app.run()
 
